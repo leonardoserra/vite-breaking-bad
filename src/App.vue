04e71786d53582f9a3f7936e1e734e54eb2d433f
leonardoserra <serra.leonardo92@gmail.com>
2023-03-29 15:27:34 +0200

    creato header, importate card da api su store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,33 @@
 <script>
+import MyHeader from './components/MyHeader.vue';
+import axios from 'axios';
+import { store } from './store.js'
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  components: {
+    MyHeader
+  },
+  methods: {
+    getCards() {
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?level=12')
+        .then(response => {
+          this.store.cards = response.data;
+          console.log(response.data);
+        })
+    }
+  },
+  created() {
+    this.getCards();
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <MyHeader />
+</template>
 
 <style scoped lang="scss"></style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+    name: 'MyHeader',
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+
+<template>
+    <div class="wrapper ">
+        <img src="/logo.png" alt="logo">
+        <h1>Yu-Gi-Oh API</h1>
+    </div>
+</template>
+
+
+
+<style scoped lang="scss">
+@use "../style/partials/_colors.scss" as*;
+
+.wrapper {
+    height: 50px;
+
+    h1 {
+        color: $tertiary-color;
+    }
+}
+</style>
