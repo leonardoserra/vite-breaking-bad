d86e1742a356f058342fcc599c1dbd3369d29704
leonardoserra <serra.leonardo92@gmail.com>
2023-03-30 15:34:07 +0200

    dice che length non è definito.. fixo dopo

--- src/components/CounterCard.vue
+++ src/components/CounterCard.vue
@@ -9,7 +9,7 @@ export default {
     },
     computed: {
         cardNumber() {
-            return store.cards.data.length;
+            // return this.store.cards.data.length;
         }
     }
 }
@@ -17,7 +17,10 @@ export default {
 
 <template>
     <div class="my-container">
-        <h3>Numero di carte trovate: {{ cardNumber }}</h3>
+        <h3>Numero di carte trovate: da fixare</h3>
+        <h3>Non sono presenti carte con questo archetipo</h3>
+        <!-- <h3 v-if="this.store.cards.data.length">Numero di carte trovate: da fixare</h3>
+            <h3 v-else>Non sono presenti carte con questo archetipo</h3> -->
     </div>
 </template>
 
