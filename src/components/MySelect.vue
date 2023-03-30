b63e460fbf1b76967e3e842922382614259e53b5
leonardoserra <serra.leonardo92@gmail.com>
2023-03-30 16:38:21 +0200

    aggiunti elementi options

--- src/components/MySelect.vue
+++ src/components/MySelect.vue
@@ -20,14 +20,32 @@ export default {
 
         <select @change="$emit('getArchetype')" id=" archetype" name="archetype" class="btn" v-model="store.selected">
             <option value="">-Filtra per archetipo-</option>
+            <option value="Abyss Actor">Abyss Actor</option>
+            <option value="Adamancipator">Adamancipator</option>
             <option value="Ancient Gear">Ancient Gear</option>
-            <option value="Ancient Warriors">Ancient Warriors</option>
+            <option value="Albaz Dragon">Albaz Dragon</option>
+            <option value="Apoqliphort">Apoqliphort</option>
+            <option value="Vaylantz">Vaylantz</option>
+            <option value="Fleur">Fleur</option>
+        <option value="Battlewasp">Battlewasp</option>
+        <option value="Blackwing">Blackwing</option>
+            <option value="Toon">Toon</option>
+            <option value="Dinomorphia">Dinomorphia</option>
             <option value="Archfiend">Archfiend</option>
-            <!-- <option v-for="(card, index) in store.cards.data" :key="index" :value="card.archetype"> -->
-            <!-- <div v-if="card.race != card[index - 1].race"> -->
-            <!-- {{ card.race }} -->
-            <!-- </div> -->
-            <!-- </option> -->
+            <option value="Despia">Despia</option>
+            <option value="Elemental HERO">Elemental HERO</option>
+            <option value="Gouki">Gouki</option>
+            <option value="Horus the Black Flame Dragon">Horus the Black Flame Dragon</option>
+            <option value="Kozmo">Kozmo</option>
+            <option value="Manadome">Manadome</option>
+            <option value="Odd-Eyes">Odd-Eyes</option>
+            <option value="Prank-Kids">Prank-Kids</option>
+            <option value="Stardust">Stardust</option>
+            <option value="Train">Train</option>
+
+            <!-- <option v-for="(card, index) in store.cards.data" :key="index" :value="card.archetype">
+                    {{ card.archetype }}
+                </option> -->
         </select>
     </div>
 </template>
